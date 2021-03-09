68e3f51f94358dfcef64cfbeee913c14e72b765a
Posiya <48229973+nknk610@users.noreply.github.com>
2021-03-09 21:37:20 +0900

    Upload Hockey and Go Model files

---
+++ Assets/TheBackend/Toolkit/MatchQueueMgr.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using BackEnd;
+
+public class MatchQueueMgr : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+
+    private void Update()
+    {
+        Backend.Match.Poll();
+    }
+}
